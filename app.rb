c0f9a0d34bcc5f8fe617a36bcb0ffcb1c66d06c6
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-16 15:08:14 -0800

    Add route to add new user

---
+++ app.rb
@@ -0,0 +1,17 @@
+require("bundler/setup")
+Bundler.require(:default)
+require('pry')
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+post('/new_user') do
+  username = params['username']
+  password = params['password']
+  password_confirmation = params['password_confirmation']
+  @user = User.create(:username => username, :password => password, :password_confirmation => password_confirmation)
+  erb(:success)
+end
