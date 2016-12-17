3e99882b1b1976132d651880092a2400cb9badc5
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-17 07:51:55 -0800

    enable log out

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ get('/') do
   erb(:index)
 end
 
+get('/signup') do
+  redirect('/')
+end
+
 post('/signup') do
   user = User.new(:username => params[:username], :password => params[:password], :password_confirmation => params[:password_confirmation])
   if user.save
@@ -22,12 +26,7 @@ post('/login') do
   @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      ## must create unique session id
-      ## i don't think it's doing that right now
-
       redirect("/success")
-      ## pass the session id to "success" page?
-      ## i don't quite understand this
     else
       redirect('/failure')
     end
@@ -40,6 +39,11 @@ end
 get('/success') do
   @user = User.find(session[:user_id])
   erb(:success)
+end
+
+get('/logout') do
+  session.clear
+  redirect('/')
 end
 
 get('/failure') do
