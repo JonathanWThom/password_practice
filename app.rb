abc02931fcf5686bec66eb29246bae235202278c
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-16 16:12:57 -0800

    reroute for new users vs old

--- app.rb
+++ app.rb
@@ -8,16 +8,40 @@ get('/') do
   erb(:index)
 end
 
-post('/user') do
+post('/sign_in') do
   username = params['username']
   password = params['password']
   password_confirmation = params['password_confirmation']
-  @user = User.create(:username => username, :password => password, :password_confirmation => password_confirmation)
-  erb(:success)
+  @user = User.new(:username => username, :password => password, :password_confirmation => password_confirmation)
+  if @user.save()
+    erb(:sign_in)
+  else
+    erb(:errors)
+  end
 end
+
+get('/sign_in') do
+  erb(:sign_in)
+end
+
+## puts username and password in url bar!!! no good
 
 get('/user') do
   @user = User.find_by(:username => params['username'])
-  @user.authenticate(params['password'])
-  erb(:success)
+  if @user
+    if @user.authenticate(params['password'])
+      erb(:user)
+    else
+      erb(:errors)
+    end
+  else
+    erb(:errors)
+  end
+end
+
+post('/user') do
+  @user = User.find(params['user_id'])
+  @user.authenticate(params['user_password'])
+  @user.update({:info => params['info']})
+  erb(:user)
 end
